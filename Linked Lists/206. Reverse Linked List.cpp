ebc2c12e8fdb413735c6b2710a68a506369f8c05
Qais-Hweidi <hweidiqais@pm.me>
2025-04-16 20:09:26 +0300

    206. Reverse Linked List

---
+++ Linked Lists/206. Reverse Linked List.cpp
@@ -0,0 +1,29 @@
+// 206. Reverse Linked List
+// iterative solution, O(n) time to iterate through the list, O(1) storage
+// Notes: we can ommit cur and just use head instead
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* reverseList(ListNode* head) {
+            ListNode* prev = NULL; // track the previous node
+            ListNode* cur = head;
+    
+            while (cur != NULL){
+                ListNode* next = cur->next; // to keep a pointer to the original list
+                cur->next = prev; // reverse the pointer
+                prev = cur;
+                cur = next;
+            }
+            return prev;
+        }
+    };
