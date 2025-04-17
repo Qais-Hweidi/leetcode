dade8ec0bd7647ea46322cbd74e7af1d290c957a
Qais-Hweidi <hweidiqais@pm.me>
2025-04-17 17:39:09 +0300

    Bad brute force solution, clock tik tok

---
+++ Linked Lists/160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,28 @@
+// 160. Intersection of Two Linked Lists
+// Conplexity: O(m*n) time, O(1) storage
+// Notes: Brute force solution for now
+// Follow up: Try other solutions with O(m+n), using 2 pointers & hash set
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+            while(headA){
+                ListNode* tempB = headB;
+                while(tempB){
+                    if (headA == tempB)
+                        return headA;
+                    tempB = tempB->next;
+                }
+            headA = headA->next;
+            }
+            return nullptr;
+        }
+    };
