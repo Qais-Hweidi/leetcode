69323570b57b90b9a791b1f1efd1b2ab68f3acc6
Qais-Hweidi <hweidiqais@pm.me>
2025-04-17 20:59:06 +0300

    zzz

---
+++ Linked Lists/387. First Unique Character in a String.cpp
@@ -0,0 +1,24 @@
+// 387. First Unique Character in a String
+// Conplexity: O(n^2) brute force, O(1) storage
+// Notes:
+// Follow up: do it with O(n)
+
+class Solution {
+    public:
+        int firstUniqChar(string s) {
+            for(int i=0; i<s.size(); i++){
+                bool flag = true;
+                for(int j=0; j<s.size(); j++){
+                    if(i == j)
+                        continue;
+                    if(s[i] == s[j]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag == true)
+                    return i;
+            }
+            return -1;
+        }
+    };
