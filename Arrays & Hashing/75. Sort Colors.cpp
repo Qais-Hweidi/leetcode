a15efae7ab3e40f13fdcfced437d6ab4a7c5ad5b
Qais-Hweidi <hweidiqais@pm.me>
2025-04-18 16:56:59 +0300

    beeeb

---
+++ Arrays & Hashing/75. Sort Colors.cpp
@@ -0,0 +1,28 @@
+// 75. Sort Colors
+// Conplexity: O(n) time, O(1) storage
+// Notes:
+// Follow up: Try solving it using vectors & pointers
+
+class Solution {
+    public:
+        void sortColors(vector<int>& nums) {
+            int zero=0, one=0, two=0;
+    
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] == 0)
+                    zero++;
+                else if(nums[i] == 1)
+                    one++;
+                else if(nums[i] == 2)
+                    two++;
+            }
+    
+            int index=0;
+            for(int i=0; i<zero; i++)
+                nums[index++] = 0;
+            for(int i=0; i<one; i++)
+                nums[index++] = 1;
+            for(int i=0; i<two; i++)
+                nums[index++] = 2;
+        }
+    };
