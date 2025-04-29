362c65f219cef3e45a0b0a7b0a8f9123f8801db8
Qais-Hweidi <hweidiqais@pm.me>
2025-04-29 23:01:02 +0300

    dummy egg

---
+++ Arrays & Hashing/242. Valid Anagram.cpp
@@ -0,0 +1,15 @@
+// 242. Valid Anagram
+// Conplexity: O(nlgn) time, O(n) storage
+// Notes: yes, i know hash map would be a more effecient solution
+// Follow up: use hash
+
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            sort(s.begin(), s.end());
+            sort(t.begin(), t.end());
+            if(s == t)
+                return true;
+            return false;
+        }
+    };
