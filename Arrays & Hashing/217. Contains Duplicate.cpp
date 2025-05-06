790d01e6a4a3738aaae4115814b3e0a7f72cc265
Qais-Hweidi <hweidiqais@pm.me>
2025-05-07 00:21:45 +0300

    shhhh

--- Arrays & Hashing/217. Contains Duplicate.cpp
+++ Arrays & Hashing/217. Contains Duplicate.cpp
@@ -1,17 +1,40 @@
 // 217. Contains Duplicate
 // Conplexity: O(nlgn) time, O(lgn) storage
-// Notes: We can do brute force but that would be n^2 which sucks, 
-// Follow up: Try solving it using hash set & hash map after you learn them
+// Notes:
+// Follow up:
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+
+//         for(int i=0; i<nums.size()-1; i++){
+//             if(nums[i] == nums[i+1])
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
+
+/***************************************************************/
+
+// 217. Contains Duplicate
+// Conplexity: O(n) time, O(n) storage
+// Notes:
+// Follow up:
 
 class Solution {
     public:
         bool containsDuplicate(vector<int>& nums) {
-            sort(nums.begin(), nums.end());
+            unordered_set<int> duplicate;
     
-            for(int i=0; i<nums.size()-1; i++){
-                if(nums[i] == nums[i+1])
+            for(int num : nums){
+                if(duplicate.contains(num))
                     return true;
+    
+                duplicate.insert(num);
             }
+    
             return false;
         }
     };
