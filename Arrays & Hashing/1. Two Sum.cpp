b8b67181cbe36b5d8e63c011543e3f205d46d3e6
Qais-Hweidi <hweidiqais@pm.me>
2025-05-08 22:24:48 +0300

    hashy

--- Arrays & Hashing/1. Two Sum.cpp
+++ Arrays & Hashing/1. Two Sum.cpp
@@ -3,15 +3,42 @@
 // Notes:
 // Follow up: Come back again and solve it with O(n)
 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for(int i = 0; i < nums.size() - 1; i++){
+//             for(int j = i + 1; j < nums.size(); j++){
+//                 if(nums[i] + nums[j] == target)
+//                     return {i, j};
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+/***************************************************************/
+
+// 1. Two Sum
+// Conplexity: O(n) time, O(n) storage
+// Notes:
+// Follow up:
+
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
-            for(int i = 0; i < nums.size() - 1; i++){
-                for(int j = i + 1; j < nums.size(); j++){
-                    if(nums[i] + nums[j] == target)
-                        return {i, j};
+            unordered_map<int, int> hashy;
+    
+            for(int i = 0; i < nums.size(); i++){
+                hashy[nums[i]] = i;
+            }
+    
+            for(int i = 0; i < nums.size(); i++){
+                int difference = target - nums[i];
+                if(hashy.contains(difference) && hashy[difference] != i){
+                    return {i, hashy[difference]};
                 }
             }
+    
             return {};
         }
     };
