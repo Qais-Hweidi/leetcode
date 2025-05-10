089b8ec2a8f6f643250faaed97b47f521c772d63
Qais-Hweidi <hweidiqais@pm.me>
2025-05-10 23:16:53 +0300

    wasup yo

---
+++ Two Pointers/125. Valid Palindrome.cpp
@@ -0,0 +1,29 @@
+// 125. Valid Palindrome
+// Conplexity: O(n) time, O(n) storage
+// Notes:
+// Follow up: Come back again and solve it with O(n)
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newS = "";
+        for (char c : s){
+            if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9'){
+                newS += tolower(c);
+            }
+        }
+
+        int l = 0;
+        int r = newS.size() - 1;
+
+        while (l < r){
+            if(newS[l] != newS[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
