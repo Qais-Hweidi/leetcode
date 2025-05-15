10a05750ed483710ed9942aeedd88ba060407808
Qais-Hweidi <hweidiqais@pm.me>
2025-05-15 23:01:43 +0300

    pew pew

---
+++ Arrays & Hashing/49. Group Anagrams.cpp
@@ -0,0 +1,23 @@
+// 49. Group Anagrams
+// Conplexity: O(n * mlgm) time, O(n * m) storage
+// Notes:
+// Follow up: There is a better way
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+
+        for (const auto& originalString : strs){
+            string sortedString = originalString;
+            sort(sortedString.begin(), sortedString.end());
+            anagramGroups[sortedString].push_back(originalString);
+        }
+
+        vector<vector<string>> groupedAnagrams;
+        for (auto& groupPair : anagramGroups){
+            groupedAnagrams.push_back(groupPair.second);
+        }
+        return groupedAnagrams;
+    }
+};
